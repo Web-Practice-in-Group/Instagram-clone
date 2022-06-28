1a85a029c8c4e847d338882778ffa80aa3705d26
Manuekle <elustondo129@gmail.com>
2022-06-28 18:58:14 -0500

    Styled components 3

--- src/assets/Elements.jsx
+++ src/assets/Elements.jsx
@@ -13,3 +13,35 @@ export const Col = styled.div`
   flex: ${(props) => props.size};
   ${(props) => props.collapse && media[props.collapse](`display: none;`)}
 `;
+
+// Navbar
+export const ContainerNav = styled.nav`
+  border-top: 1px solid grey;
+  border-bottom: 1px solid grey;
+  flex-direction: column;
+  
+  display: flex;
+  top: 0;
+  width: 100%;
+`;
+
+export const LogoNav = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: #fff;
+  background-color: #000;
+  border-bottom: 1px solid grey;
+  border-top: 1px solid grey;
+  border-left: 1px solid grey;
+  border-right: 1px solid grey;
+  border-radius: 10px;
+  cursor: pointer;
+  &:hover {
+    background-color: #fff;
+    color: #000;
+  }
+`;
--- src/components/Nav/Navigation.jsx
+++ src/components/Nav/Navigation.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 // ?Iconos
 
-import { Grid, Row, Col } from "../../assets/Elements";
+import { Col, Row, ContainerNav, LogoNav} from "../../assets/Elements";
 
 function Navigation() {
   const [mode, setMode] = useState();
@@ -12,22 +12,13 @@ function Navigation() {
   }
 
   return (
-    <Grid>
+    <ContainerNav style={{ alignItems: "center" }}>
       <Row>
-        <Col size={1} style={{ border: "1px solid red" }}>
-          1
-        </Col>
-        <Col size={2} style={{ border: "1px solid red" }}>
-          2
-        </Col>
-        <Col size={1} collapse="xs" style={{ border: "1px solid red" }}>
-          disabled
-        </Col>
-        <Col size={1} style={{ border: "1px solid red" }}>
-          1
-        </Col>
-      </Row>
-    </Grid>
+        <Col size={1}>
+         <LogoNav>a</LogoNav>
+        </Col>                     
+      </Row>      
+    </ContainerNav>
   );
 }
 
