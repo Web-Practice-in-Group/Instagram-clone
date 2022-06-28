fdd09be8b04d14db628bfc07bed41cc8dee03857
Manuekle <elustondo129@gmail.com>
2022-06-28 18:34:33 -0500

    Styled components 2

--- src/assets/Elements.jsx
+++ src/assets/Elements.jsx
@@ -1,7 +1,15 @@
 import styled from "styled-components";
 
-export const Nav = styled.nav`
-  flex-basis: 100%;
-  order: 0;
-  padding: 0;
+export const Grid = styled.div``;
+export const Row = styled.div`
+  display: flex;
+`;
+
+const media = {
+  xs: (styles) => `@media (max-width: 480px) { ${styles} }`,
+};
+
+export const Col = styled.div`
+  flex: ${(props) => props.size};
+  ${(props) => props.collapse && media[props.collapse](`display: none;`)}
 `;
--- src/components/Nav/Navigation.jsx
+++ src/components/Nav/Navigation.jsx
@@ -1,21 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
-import { Grid } from "@nextui-org/react";
-import { Link } from "react-router-dom";
-import DarkMode from "../DarkMode/DarkMode";
-import Search from "./Search";
 // ?Iconos
-import {
-  CommentIcon,
-  DotsIcon,
-  HomeIcon,
-  LikeIcon,
-  MessageIcon,
-  SaveIcon,
-  UserIcon,
-} from "../Breadcrumbs/Icons";
 
-import {Nav} from "../../assets/Elements";
+import { Grid, Row, Col } from "../../assets/Elements";
 
 function Navigation() {
   const [mode, setMode] = useState();
@@ -25,62 +12,22 @@ function Navigation() {
   }
 
   return (
-    <div>
-      <Nav></Nav>
-      <Grid.Container
-        justify="center"
-        css={{
-          borderBottom: "$space$1 solid $gray200",
-          backgroundColor: {mode}, // default background color
-        }}
-      >
-        <Grid
-          css={{
-            border: "$space$1 solid $gray200",
-          }}
-        >
-          <img
-            src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
-            alt="logo"
-          />
-        </Grid>
-        <Grid
-          css={{
-            border: "$space$1 solid $gray200",
-          }}
-        >
-          <Search />
-          <DarkMode setMode={setMode} />
-        </Grid>
-        <Grid
-          css={{
-            border: "$space$1 solid $gray200",
-          }}
-        >
-          <Link to="/">
-            <CommentIcon color={mode} />
-          </Link>
-          <Link to="/">
-            <DotsIcon fill={mode} />
-          </Link>
-          <Link to="/">
-            <HomeIcon fill={mode} />
-          </Link>
-          <Link to="/">
-            <LikeIcon fill={mode} />
-          </Link>
-          <Link to="/">
-            <MessageIcon color={mode} fill={mode} />
-          </Link>
-          <Link to="/">
-            <SaveIcon color={mode} />
-          </Link>
-          <Link to="/">
-            <UserIcon fill={mode} />
-          </Link>
-        </Grid>
-      </Grid.Container>
-    </div>
+    <Grid>
+      <Row>
+        <Col size={1} style={{ border: "1px solid red" }}>
+          1
+        </Col>
+        <Col size={2} style={{ border: "1px solid red" }}>
+          2
+        </Col>
+        <Col size={1} collapse="xs" style={{ border: "1px solid red" }}>
+          disabled
+        </Col>
+        <Col size={1} style={{ border: "1px solid red" }}>
+          1
+        </Col>
+      </Row>
+    </Grid>
   );
 }
 
